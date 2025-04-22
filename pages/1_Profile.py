95b143618bf32be4444a794b9e1a5e240d5f05ba
ProgrammerJAK03 <junaid30023080@gmail.com>
2025-04-22 16:55:02 +0500

    Update

--- pages/1_Profile.py
+++ pages/1_Profile.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from db import fetch_user
+import base64
+from io import BytesIO
 
 st.title("👤 User Profile")
 
@@ -7,8 +9,19 @@ user = fetch_user()
 
 if user:
     name, phone, image = user
-    st.image(image, width=150)
+
+    try:
+        # Convert base64 to image bytes
+        image_bytes = base64.b64decode(image)
+        image_file = BytesIO(image_bytes)
+
+        # Display image
+        st.image(image_file, width=150)
+    except:
+        st.warning("Image could not be loaded.")
+
     st.write("**Name:**", name)
     st.write("**Phone:**", phone)
 else:
     st.warning("No user data found.")
+
